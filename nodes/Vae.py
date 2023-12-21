bdbe3db2a972fa0d38153da045df66511fc293d8
shadowcz007 <chizhiwei007@163.com>
2023-12-21 10:39:32 +0800

    Update Vae.py

--- nodes/Vae.py
+++ nodes/Vae.py
@@ -145,7 +145,7 @@ class VAELoader:
     RETURN_TYPES = ("VAE",)
     FUNCTION = "load_vae"
 
-    CATEGORY = "♾️Mixlab/ConsistencyDecoder"
+    CATEGORY = "♾️Mixlab/_test"
 
     #TODO: scale factor?
     def load_vae(self, vae_name):
@@ -165,7 +165,7 @@ class VAEDecode:
     RETURN_TYPES = ("IMAGE",)
     FUNCTION = "decode"
 
-    CATEGORY = "♾️Mixlab/ConsistencyDecoder"
+    CATEGORY = "♾️Mixlab/_test"
 
     def decode(self, vae, samples):
         image = vae.decode(samples["samples"].to("cuda:0"))
