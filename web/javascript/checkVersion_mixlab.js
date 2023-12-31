cfc218961699c70d843017151e862a5f2cdf1f2c
shadowcz007 <chizhiwei007@163.com>
2023-12-31 23:58:16 +0800

    v0.8.1

--- web/javascript/checkVersion_mixlab.js
+++ web/javascript/checkVersion_mixlab.js
@@ -3,7 +3,7 @@ import { app } from '../../../scripts/app.js'
 const repoOwner = 'shadowcz007' // 替换为仓库的所有者
 const repoName = 'comfyui-mixlab-nodes' // 替换为仓库的名称
 
-const version = 'v0.8.0'
+const version = 'v0.8.1'
 
 fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/releases/latest`)
   .then(response => response.json())
